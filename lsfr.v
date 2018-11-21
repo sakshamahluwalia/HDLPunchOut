4538a10508cf07c1ac0a1a2d180ea2bc5311172e
jai-aggarwal <jai.aggarwal@mail.utoronto.ca>
2018-11-20 23:01:37 -0500

    Finished prelim lsfr for Milestone 2 Task 2, framework for VGA code

---
+++ lsfr.v
@@ -0,0 +1,70 @@
+module LFSR(clock, reset_n, enable, random_pos);
+
+	input clock;
+	input reset_n;
+	// we need enable so we can set a random seed for the LFSR
+	input enable;
+	
+	// 1 bit position of XOR'd values
+	output random_pos;
+	
+	// stores total value in LSFR
+	reg [7:0] LFSR_val;
+	
+	// random values
+	wire end_val = LFSR_val[3] ^ LFSR_val[7];
+	
+	// counter value helps find seed for LFSR
+	wire [7:0] counter_val;
+	
+	// flag to determine whether the LFSR seed value has been set
+	// necessary to allow it to be pseudo-random
+	reg LFSR_set;
+	
+	// constantly running to create random initial value
+	counter c0(
+			.clock(clock),
+			.reset_n(reset_n),
+	      .out(counter_val)
+	);
+	
+	always @(posedge clock)
+		begin
+			// on reset we can use any value, set flag to 0 (not set)
+			if (!reset_n)
+				LFSR_val <= 8'b00001010;
+				LFSR_set <= 1'b0;
+			// while enable is off and flag = 0, we can update 
+			// the LSFR value to be a random seed
+			else if (!enable & !LSFR_set)
+				LFSR_val <= (counter_val != 8'b0) counter_val ? 8'b00001010;
+				LFSR_set <= 1'b1;
+			// when we need the LSFR to work, we can generate our pseudorandom values
+			else if (enable)
+				LFSR_val <= {LFSR_val[6], LFSR_val[5], LFSR_val[4], LFSR_val[3],
+								 LFSR_val[2], LFSR_val[1], LFSR_val[0], end_val};
+		end
+	
+	//end val will be pseudorandom, so this is all we need
+	random_pos = end_val;
+		
+	
+endmodule
+
+module counter(clock, reset_n, out);
+
+	input clock;
+	input reset_n;
+	output reg [7:0] out;
+	
+	always @(posedge clock)
+	begin
+		if (!reset_n)
+			begin
+				out <= 8'b0;
+			end
+		else
+			out <= out + 1'b1;
+	end
+			
+endmodule
---
+++ overall_vga.v
@@ -0,0 +1,55 @@
+module overall_vga(
+		CLOCK_50,						//	On Board 50 MHz
+		// Your inputs and outputs here
+        KEY,
+        SW,
+		// The ports below are for the VGA output.  Do not change.
+		VGA_CLK,   						//	VGA Clock
+		VGA_HS,							//	VGA H_SYNC
+		VGA_VS,							//	VGA V_SYNC
+		VGA_BLANK_N,						//	VGA BLANK
+		VGA_SYNC_N,						//	VGA SYNC
+		VGA_R,   						//	VGA Red[9:0]
+		VGA_G,	 						//	VGA Green[9:0]
+		VGA_B   						//	VGA Blue[9:0]
+	);
+	
+	input	  CLOCK_50;			
+	
+	//are we using these?
+	input   [9:0]   SW;
+	input   [3:0]   KEY;
+
+
+	// Do not change the following outputs
+	output			VGA_CLK;   				//	VGA Clock
+	output			VGA_HS;					//	VGA H_SYNC
+	output			VGA_VS;					//	VGA V_SYNC
+	output			VGA_BLANK_N;				//	VGA BLANK
+	output			VGA_SYNC_N;				//	VGA SYNC
+	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
+	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
+	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
+	
+	vga_adapter VGA(
+			.resetn(resetn),
+			.clock(CLOCK_50),
+			.colour(colour),
+			.x(x),
+			.y(y),
+			.plot(writeEn),
+			/* Signals for the DAC to drive the monitor. */
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK_N),
+			.VGA_SYNC(VGA_SYNC_N),
+			.VGA_CLK(VGA_CLK));
+		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.MONOCHROME = "FALSE";
+		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+		defparam VGA.BACKGROUND_IMAGE = "black.mif";
+
+endmodule
