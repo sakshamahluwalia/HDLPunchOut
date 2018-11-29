4807615c9b09b1a0cb019fc61db82a11dbdf9554
jai-aggarwal <jai.aggarwal@mail.utoronto.ca>
2018-11-29 15:29:20 -0500

    Stuff from lab last night with working sprites

--- overall_vga.v
+++ overall_vga.v
@@ -27,8 +27,7 @@ module overall_vga(
 	wire [7:0] x;
 	wire [6:0] y;
 	wire writeEn;
-	
-   assign writeEn = 1'b1;
+
 //	assign x = 8'b00000000;
 //	assign y = 7'b0000000;
 //	assign colour = 3'b011;
@@ -69,14 +68,14 @@ module overall_vga(
 	// it is complete
 	wire start_project;
 	
-	
+	wire en_enable = 1'b1;
 	//two resets
-	
+	initialization io(CLOCK_50, writeEn, resetn, en_enable, start_project, x, y, colour);
 	//resetn is the overall system reset to bring everything back to beginning
 	wire resetn = KEY[0];
 	//interim_reset is the reset that will be ongoing while the initialization
 	// module is running, to ensure nothing starts before we want it to
-	wire interim_reset = resetn & start_project;
+	//wire interim_reset = resetn & start_project;
 	
 	//enemy_attributes
 	
@@ -123,24 +122,98 @@ module overall_vga(
 //			.move(lfsrEnable), 
 //			.attack_out(initiate_attack)
 //	);
-	initialization i0(
-		.clock(clock),
-		.reset_n(resetn),
-		.start_project(start_project)
-		.x_out(x),
-		.y_out(y),
-		.c_out(colour)
-	);
-		
+
 
 
 endmodule
 
-module initialization(clock, reset_n, start_project, x_out, y_out, c_out);
+module initialization(clock, writeEn, reset_n, enable, start_project, x_out, y_out, c_out);
 	input clock;
 	input reset_n;
+	input enable;
 	
-	output start_project;
+	
+	output reg start_project;
+	output reg [7:0] x_out;
+	output reg [6:0] y_out;
+	output reg [2:0] c_out;
+	output reg writeEn;
+	
+	reg [11:0] counter;
+	reg [10:0] address_counter;
+	reg en_enable;
+	
+	wire start_player;
+	wire real_start;
+	wire [7:0] enemy_x;
+	wire [6:0] enemy_y;
+	wire [2:0] enemy_col;
+	wire [7:0] player_x;
+	wire [6:0] player_y;
+	wire [2:0] player_col;
+	wire [7:0] left_x;
+	wire [6:0] left_y;
+	wire [2:0] left_col;
+	wire [7:0] right_x;
+	wire [6:0] right_y;
+	wire [2:0] right_col;
+	
+	
+	always @(*)
+		begin
+			if (!reset_n)
+				begin
+					start_project <= 1'b0;
+					writeEn <= 1'b0;
+				end
+			else if (real_start)
+				begin
+					writeEn <= 1'b1;
+				end
+			else if (start_right)
+				begin
+					en_enable <= 1'b0;
+					x_out <= right_x;
+					y_out <= right_y;
+					c_out <= right_col;
+				end
+			else if (start_player)
+				begin
+					en_enable <= 1'b0;
+					x_out <= player_x;
+					y_out <= player_y;
+					c_out <= player_col;
+				end
+			else if (start_left)
+				begin
+					en_enable <= 1'b0;
+					x_out <= left_x;
+					y_out <= left_y;
+					c_out <= left_col;
+				end	
+			else if (enable)
+				begin
+					en_enable <= enable;
+					x_out <= enemy_x;
+					y_out <= enemy_y;
+					c_out <= enemy_col;
+				end
+			
+		end
+	initialize_enemy(clock, reset_n, en_enable, start_left, enemy_x, enemy_y, enemy_col);
+	initialize_left(clock, reset_n, start_left, start_player, left_x, left_y, left_col);
+	initialize_player(clock, reset_n, start_player, start_right, player_x, player_y, player_col);
+	initialize_right(clock, reset_n, start_right, real_start, right_x, right_y, right_col);
+
+	
+endmodule
+
+module initialize_enemy(clock, reset_n, enable, start_next, x_out, y_out, c_out);
+	input clock;
+	input reset_n;
+	input enable;
+	
+	output reg start_next;
 	output [7:0] x_out;
 	output [6:0] y_out;
 	output [2:0] c_out;
@@ -148,121 +221,295 @@ module initialization(clock, reset_n, start_project, x_out, y_out, c_out);
 	reg [11:0] counter;
 	reg [10:0] address_counter;
 	
-	wire [2:0] c_out;
-	
-	
-	boxerRam	boxerRam_inst (
+	back	back_inst (
 			.address(address_counter),
 			.clock(clock),
-			.data(3'b000),
+			.data(3'b111),
 			.wren(1'b0),
 			.q(c_out)
 	);
 	
-	always @(posedge clock) 
+	always @(posedge clock)
 		begin
-		if (!resetn)
-			begin
-				counter <= 12'b0000_0000_0000;
-				address_counter <= 11'b000_0000_0000;
-			end
-		else if (counter == 12'b100111100111)
-			begin
-				counter <= 12'b0000_0000_0000;
-				address_counter <= 11'b000_0000_0000; 
-			end
-		else if (counter[5:0] == 6'b100111)
-			begin
-				counter <= counter + 7'b100_0000;
-				address_counter <= address_counter + 1'b1;
-			end	
-		else
-			begin
-				counter <= counter + 1'b1;
-				address_counter <= address_counter + 1'b1; 
-			end
+			if (enable)
+				begin
+				if (!reset_n)
+					begin
+						counter <= 12'b0000_0000_0000;
+						address_counter <= 11'b000_0000_0000;
+						start_next <= 1'b0;
+
+					end
+				else if (counter == 12'b100111100111)
+					begin
+						counter <= 12'b0000_0000_0000;
+						address_counter <= 11'b000_0000_0000;
+						start_next <= 1'b1;
+			
+					end
+				else if (counter[5:0] == 6'b100111)
+					begin
+						counter <= {counter[11:6] + 1'b1, 6'b000000};
+						address_counter <= address_counter + 1'b1;
+					end	
+				else
+					begin
+						counter <= counter + 1'b1;
+						address_counter <= address_counter + 1'b1; 
+					end
+				end
 		end
 
-	assign x_out = 8'b0011_1100 + counter[5:0];
+	assign x_out = 8'b0000_1100 + counter[5:0];
 	assign y_out = 7'b000_0110 + counter[11:6];
 	
-	assign start_project = 1'b0;
-	
-	
-	
 	
 endmodule
 
 
-
-module enemy_x_pos_tracker(clock, reset_n, x_pos_change, enemy_curr_x);
+module initialize_player(clock, reset_n, enable, start_next, x_out, y_out, c_out);
 	input clock;
 	input reset_n;
-	input x_pos_change;
+	input enable;
 	
-	output reg [7:0] enemy_curr_x;
+	output reg start_next;
+	output [7:0] x_out;
+	output [6:0] y_out;
+	output [2:0] c_out;
+	
+	reg [11:0] counter;
+	reg [10:0] address_counter;
+	
+	back	back_inst (
+			.address(address_counter),
+			.clock(clock),
+			.data(3'b111),
+			.wren(1'b0),
+			.q(c_out)
+	);
 	
 	always @(posedge clock)
 		begin
-			if (!reset_n)
-				enemy_curr_x <= 8'b?;
-			// TODO FIND NUMBERS
-			else if (x_pos_change == 24234234234)
-				enemy_curr_x <= x_pos_change;
-			else if (x_pos_change == 24234234234)
-				enemy_curr_x <= x_pos_change;
-			else if (x_pos_change == 24234234234)
-				enemy_curr_x <= x_pos_change;
+			if (enable)
+				begin
+				if (!reset_n)
+					begin
+						counter <= 12'b0000_0000_0000;
+						address_counter <= 11'b000_0000_0000;
+						start_next <= 1'b0;
+
+					end
+				else if (counter == 12'b100111100111)
+					begin
+						counter <= 12'b0000_0000_0000;
+						address_counter <= 11'b000_0000_0000;
+						start_next <= 1'b1;
+			
+					end
+				else if (counter[5:0] == 6'b100111)
+					begin
+						counter <= {counter[11:6] + 1'b1, 6'b000000};
+						address_counter <= address_counter + 1'b1;
+					end	
+				else
+					begin
+						counter <= counter + 1'b1;
+						address_counter <= address_counter + 1'b1; 
+					end
+				end
 		end
+
+	assign x_out = 8'b0010_1000 + counter[5:0];
+	assign y_out = 7'b100_0110 + counter[11:6];
+	
+	
 endmodule
 
-module left_hand_y_pos_tracker(clock, reset_n, left_y_pos_change, left_hand_curr_y);
+module initialize_left(clock, reset_n, enable, start_next, x_out, y_out, c_out);
 	input clock;
 	input reset_n;
-	input left_y_pos_change;
+	input enable;
 	
-	output reg [6:0] left_hand_curr_y;
+	output reg start_next;
+	output [7:0] x_out;
+	output [6:0] y_out;
+	output [2:0] c_out;
+	
+	reg [11:0] counter;
+	reg [10:0] address_counter;
+	
+	back	back_inst (
+			.address(address_counter),
+			.clock(clock),
+			.data(3'b111),
+			.wren(1'b0),
+			.q(c_out)
+	);
 	
 	always @(posedge clock)
 		begin
-			if (!reset_n)
-				enemy_curr_x <= 2'b01;
-			else
-				enemy_curr_x <= x_pos_change;
+			if (enable)
+				begin
+				if (!reset_n)
+					begin
+						counter <= 12'b0000_0000_0000;
+						address_counter <= 11'b000_0000_0000;
+						start_next <= 1'b0;
+
+					end
+				else if (counter == 12'b100111100111)
+					begin
+						counter <= 12'b0000_0000_0000;
+						address_counter <= 11'b000_0000_0000;
+						start_next <= 1'b1;
+			
+					end
+				else if (counter[5:0] == 6'b100111)
+					begin
+						counter <= {counter[11:6] + 1'b1, 6'b000000};
+						address_counter <= address_counter + 1'b1;
+					end	
+				else
+					begin
+						counter <= counter + 1'b1;
+						address_counter <= address_counter + 1'b1; 
+					end
+				end
 		end
+
+	assign x_out = 8'b0000_0000 + counter[5:0];
+	assign y_out = 7'b100_0110 + counter[11:6];
+	
+	
 endmodule
 
-module right_hand_y_pos_tracker(clock, reset_n, right_y_pos_change, right_hand_curr_y);
+module initialize_right(clock, reset_n, enable, start_next, x_out, y_out, c_out);
 	input clock;
 	input reset_n;
-	input right_y_pos_change;
+	input enable;
 	
-	output reg [6:0] right_hand_curr_y;
+	output reg start_next;
+	output [7:0] x_out;
+	output [6:0] y_out;
+	output [2:0] c_out;
+	
+	reg [11:0] counter;
+	reg [10:0] address_counter;
+	
+	back	back_inst (
+			.address(address_counter),
+			.clock(clock),
+			.data(3'b111),
+			.wren(1'b0),
+			.q(c_out)
+	);
 	
 	always @(posedge clock)
 		begin
-			if (!reset_n)
-				enemy_curr_x <= 2'b01;
-			else
-				enemy_curr_x <= x_pos_change;
+			if (enable)
+				begin
+				if (!reset_n)
+					begin
+						counter <= 12'b0000_0000_0000;
+						address_counter <= 11'b000_0000_0000;
+						start_next <= 1'b0;
+
+					end
+				else if (counter == 12'b100111100111)
+					begin
+						counter <= 12'b0000_0000_0000;
+						address_counter <= 11'b000_0000_0000;
+						start_next <= 1'b1;
+			
+					end
+				else if (counter[5:0] == 6'b100111)
+					begin
+						counter <= {counter[11:6] + 1'b1, 6'b000000};
+						address_counter <= address_counter + 1'b1;
+					end	
+				else
+					begin
+						counter <= counter + 1'b1;
+						address_counter <= address_counter + 1'b1; 
+					end
+				end
 		end
+
+	assign x_out = 8'b0101_0000 + counter[5:0];
+	assign y_out = 7'b100_0110 + counter[11:6];
+	
+	
 endmodule
 
-module main_player_x_pos_tracker(clock, reset_n, y_pos_change, main_player_curr_x);
-	input clock;
-	input reset_n;
-	input y_pos_change;
-	
-	output reg [6:0] left_hand_curr_y;
-	
-	always @(posedge clock)
-		begin
-			if (!reset_n)
-				enemy_curr_x <= 2'b01;
-			else
-				enemy_curr_x <= x_pos_change;
-		end
-endmodule
+//
+//module enemy_x_pos_tracker(clock, reset_n, x_pos_change, enemy_curr_x);
+//	input clock;
+//	input reset_n;
+//	input x_pos_change;
+//	
+//	output reg [7:0] enemy_curr_x;
+//	
+//	always @(posedge clock)
+//		begin
+//			if (!reset_n)
+//				enemy_curr_x <= 8'b?;
+//			// TODO FIND NUMBERS
+//			else if (x_pos_change == 24234234234)
+//				enemy_curr_x <= x_pos_change;
+//			else if (x_pos_change == 24234234234)
+//				enemy_curr_x <= x_pos_change;
+//			else if (x_pos_change == 24234234234)
+//				enemy_curr_x <= x_pos_change;
+//		end
+//endmodule
+//
+//module left_hand_y_pos_tracker(clock, reset_n, left_y_pos_change, left_hand_curr_y);
+//	input clock;
+//	input reset_n;
+//	input left_y_pos_change;
+//	
+//	output reg [6:0] left_hand_curr_y;
+//	
+//	always @(posedge clock)
+//		begin
+//			if (!reset_n)
+//				enemy_curr_x <= 2'b01;
+//			else
+//				enemy_curr_x <= x_pos_change;
+//		end
+//endmodule
+//
+//module right_hand_y_pos_tracker(clock, reset_n, right_y_pos_change, right_hand_curr_y);
+//	input clock;
+//	input reset_n;
+//	input right_y_pos_change;
+//	
+//	output reg [6:0] right_hand_curr_y;
+//	
+//	always @(posedge clock)
+//		begin
+//			if (!reset_n)
+//				enemy_curr_x <= 2'b01;
+//			else
+//				enemy_curr_x <= x_pos_change;
+//		end
+//endmodule
+//
+//module main_player_x_pos_tracker(clock, reset_n, y_pos_change, main_player_curr_x);
+//	input clock;
+//	input reset_n;
+//	input y_pos_change;
+//	
+//	output reg [6:0] left_hand_curr_y;
+//	
+//	always @(posedge clock)
+//		begin
+//			if (!reset_n)
+//				enemy_curr_x <= 2'b01;
+//			else
+//				enemy_curr_x <= x_pos_change;
+//		end
+//endmodule
 
 	
 
